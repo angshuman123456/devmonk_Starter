05c640931dc1802828ea3db030eabd033967d8f8
angshuman123456 <angshuman.halder243@gmail.com>
2018-04-29 17:14:15 +0530

    created a background image

--- controllers/home.js
+++ controllers/home.js
@@ -1,13 +1,12 @@
 module.exports = {
-    setRouting : function(router){
-        router.get('/',this.getHomePage);
-            
+    setRouting: function(router) {
+        router.get('/', this.getHomePage);
+        router.get('/about', this.getAbout);
     },
-
-    getHomePage : function(req,res){
-       
+    getHomePage: function(req, res) {
         res.render('page/home');
     },
-}
-
-  
+    getAbout: function(req, res) {
+        res.render('page/aboutUs');
+    },
+};
--- server.js
+++ server.js
@@ -1,50 +1,49 @@
-'use strict'
+'use strict';
 
-//NATIVE MODULES
+// NATIVE MODULES
 const path = require('path');
-//EXTERNAL MODULES
+
+// EXTERNAL MODULES
 const express = require('express');
 const engine = require('ejs-mate');
-//OWN MODULES
-const homeController = require('./controllers/home');
 
+// OWN MODULES
+const homeController = require('./controllers/home');
 
 setupExpress();
 
-function setupExpress(){
-
-    // CREATING AN INSTACE OF EXPRESS 
+/**
+ * sets up express middleware
+ */
+function setupExpress() {
+    // CREATING AN INSTACE OF EXPRESS
     const app = express();
     const port = process.env.PORT || 3000;
- 
+
     configureExpress(app);
 
     // IN FUTURE WE CAN OPT TO USE  'express-promise-router' INSTEAD
-    const router = express.Router();
-  
+    const router = express.Router(); // eslint-disable-line new-cap
 
     homeController.setRouting(router);
     app.use(router);
 
 
-    app.listen( port , (err,result)=>{
-        if(err) return console.log('server listen failed',err);
+    app.listen( port, (err, result) => {
+        if (err) return console.log('server listen failed', err);
         console.log(`listening on ${port}`);
-    })
+    });
 }
 
-function configureExpress(app){
+/**
+ * Configures express (setting up engine and other middlewares)
+ * @param {Object} app - express object
+ */
+function configureExpress(app) {
+    // VIEW ENGINE SETUP
+    app.engine('ejs', engine);
+    app.set('view engine', 'ejs');
 
-    //VIEW ENGINE SETUP
-    app.engine('ejs',engine);
-    app.set('view engine','ejs');
-
-    //MIDDLEWARES
-    app.use(express.static(path.join(__dirname,'public')));
+    // MIDDLEWARES
+    app.use(express.static(path.join(__dirname, 'public')));
 }
-
-
-
-
-
-
