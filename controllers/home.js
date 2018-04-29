d85e6ff66f83ec43e38a44241ba4b34c87c2a5c5
angshuman123456 <angshuman.halder243@gmail.com>
2018-04-29 21:19:08 +0530

    code of conduct and home page

--- controllers/home.js
+++ controllers/home.js
@@ -2,11 +2,15 @@ module.exports = {
     setRouting: function(router) {
         router.get('/', this.getHomePage);
         router.get('/about', this.getAbout);
+        router.get('/codeOfConduct', this.getCodeOfConduct);
     },
     getHomePage: function(req, res) {
         res.render('page/home');
     },
     getAbout: function(req, res) {
         res.render('page/aboutUs');
+    },
+    getCodeOfConduct: function(req, res) {
+        res.render('page/codeOfConduct');
     },
 };
