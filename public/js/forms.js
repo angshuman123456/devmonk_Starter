6498843649bd73b0e3e6fbe330e5929662bff4dc
angshuman123456 <angshuman.halder243@gmail.com>
2018-05-14 01:26:35 +0530

    created forms for the membership of college, student and professional

---
+++ public/js/forms.js
@@ -0,0 +1,28 @@
+// let studentHomeTown = document.querySelector('[name="homeTown"]');
+// let areaOfExpertise = document.querySelector('[name="areaOfExpertise"]');
+// let college = document.querySelector('[name="college"]');
+// let collegeWithAddress = document.querySelector('[name="collegeWithAddress"]');
+
+// let formTabs = document.querySelector('.form__tabs');
+// formTabs.addEventListener('click', selectTab);
+// areaOfExpertise.style.display = 'none';
+// collegeWithAddress.style.display = 'none';
+
+
+// /**
+//  * @param {Object} e - event object that is implicitly passed
+//  */
+// function selectTab(e) {
+//   e.preventDefault();
+//   if (e.target.classList.contains('student')) {
+//     studentHomeTown.style.display = 'block';
+//     areaOfExpertise.style.display = 'none';
+//     collegeWithAddress.style.display = 'none';
+//     college.style.display = 'block';
+//   } else if (e.target.classList.contains('professional')) {
+//     studentHomeTown.style.display = 'none';
+//     areaOfExpertise.style.display = 'block';
+//     collegeWithAddress.style.display = 'block';
+//     college.style.display = 'none';
+//   }
+// }
