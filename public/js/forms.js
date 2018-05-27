e1f97fbdb44c073320f148dbc0c0522b434c53cf
angshuman123456 <angshuman.halder243@gmail.com>
2018-05-27 22:08:11 +0530

    completed events and member ship cards

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,39 +1,10 @@
-// let studentHomeTown = document.querySelector('[name="homeTown"]');
-// let areaOfExpertise = document.querySelector('[name="areaOfExpertise"]');
-// let college = document.querySelector('[name="college"]');
-// let collegeWithAddress = document.querySelector('[name="collegeWithAddress"]');
+$('.button').click(function() {
+  let buttonId = $(this).attr('id');
+  $('#modal-container').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+});
 
-// let formTabs = document.querySelector('.form__tabs');
-// formTabs.addEventListener('click', selectTab);
-// areaOfExpertise.style.display = 'none';
-// collegeWithAddress.style.display = 'none';
-
-
-// /**
-//  * @param {Object} e - event object that is implicitly passed
-//  */
-// function selectTab(e) {
-//   e.preventDefault();
-//   if (e.target.classList.contains('student')) {
-//     studentHomeTown.style.display = 'block';
-//     areaOfExpertise.style.display = 'none';
-//     collegeWithAddress.style.display = 'none';
-//     college.style.display = 'block';
-//   } else if (e.target.classList.contains('professional')) {
-//     studentHomeTown.style.display = 'none';
-//     areaOfExpertise.style.display = 'block';
-//     collegeWithAddress.style.display = 'block';
-//     college.style.display = 'none';
-//   }
-// }
-
-$('.button').click(function(){
-    var buttonId = $(this).attr('id');
-    $('#modal-container').removeAttr('class').addClass(buttonId);
-    $('body').addClass('modal-active');
-  })
-  
-  $('#modal-container').click(function(){
-    $(this).addClass('out');
-    $('body').removeClass('modal-active');
-  });
+$('#modal-container').click(function() {
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
+});
