2c6e65c9a6cabe6ccc4e18782690db7b9d5f53ed
angshuman123456 <angshuman.halder243@gmail.com>
2018-05-30 01:09:29 +0530

    completed advisory council isotope filters

---
+++ public/js/advisoryCouncil.js
@@ -0,0 +1,20 @@
+
+$('.all-btn').click(function(e) {
+  e.preventDefault();
+  $('.grid').isotope({filter: '.all'});
+});
+
+$('.national-btn').click(function(e) {
+  e.preventDefault();
+  $('.grid').isotope({filter: '.national'});
+});
+
+$('.gwalior-btn').click(function(e) {
+  e.preventDefault();
+  $('.grid').isotope({filter: '.gwalior'});
+});
+
+$('.hummits-btn').click(function(e) {
+  e.preventDefault();
+  $('.grid').isotope({filter: '.HUMMITS'});
+});
