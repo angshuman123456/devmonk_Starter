7e8def06b3134f3c4bb1295c7c25715f5daebdfc
angshuman123456 <angshuman.halder243@gmail.com>
2018-06-04 23:52:15 +0530

    added the contact us section to show up on navbar visibility

--- public/js/purejavascript.js
+++ public/js/purejavascript.js
@@ -11,20 +11,20 @@
  * pure JavaScript version
 **/
 
-var bar_bg = document.getElementById("scrollbar-bg"),
+var bar_bg = document.getElementById('scrollbar-bg'),
     body = document.body,
     html = document.documentElement;
 
-bar_bg.style.minWidth = document.width + "px";
+bar_bg.style.minWidth = document.width + 'px';
 
-document.getElementsByTagName("body")[0].onresize = function() {
+document.getElementsByTagName('body')[0].onresize = function() {
 	// Update the gradient width
-	bar_bg.style.minWidth = document.width + "px";
+	bar_bg.style.minWidth = document.width + 'px';
 }
 
 window.onscroll = function() {
 	// Change the width of the progress bar
-	var bar = document.getElementById("scrollbar"),
+	var bar = document.getElementById('scrollbar'),
 		dw  = document.documentElement.clientWidth,
 		dh  = Math.max( body.scrollHeight, body.offsetHeight, 
                        html.clientHeight, html.scrollHeight, html.offsetHeight ),
@@ -33,5 +33,16 @@ window.onscroll = function() {
                               document.documentElement.scrollTop : document.body.scrollTop),
 		bw  = ((pos / (dh - wh)) * 100);
 
-	bar.style.width = bw + "%";
+	bar.style.width = bw + '%';
+}
+
+let toggleNav = document.querySelector('#navi-toggle');
+
+toggleNav.addEventListener('click', function() {
+	console.log('asdf');
+	$('.contact ').fadeIn();
+});
+
+if (toggleNav.checked == true) {
+
 }
--- public/js/removeFooter.js
+++ public/js/removeFooter.js
@@ -1,5 +1,7 @@
 $(window).scroll(function() {
-  if ($(this).scrollTop() > 0) {
+  let checked = document.querySelector('#navi-toggle').checked;
+  if (($(this).scrollTop() > 0 && !checked)) {
+    console.log($(this).scrollTop() + $(this).height() == $(document).height());
     $('.contact').fadeOut();
   } else {
     $('.contact').fadeIn();
